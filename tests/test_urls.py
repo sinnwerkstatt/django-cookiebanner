589d1934d0767fb9e6489887bb3724ad8b2073cb
Andreas Nüßlein <andreas@nuessle.in>
2024-02-20 09:31:41 +0000

    run black, poetry update, cleanup pyproject.toml

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-
-urlpatterns = [
-    path('', TemplateView.as_view, template_name='base'),
-]
+urlpatterns = [path("", TemplateView.as_view(template_name="base.html"))]
